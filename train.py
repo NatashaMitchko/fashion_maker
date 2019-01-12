92aeddcaaa8f94419c7f395a7bc2fb48ebad0a32
Natasha Mitchko <natashamitchko@gmail.com>
2019-01-12 16:49:11 -0500

    define generator and discriminator

---
+++ train.py
@@ -0,0 +1,62 @@
+import tensorflow as tf
+from tensorflow import keras
+
+BATCH_SIZE = 64
+
+def process_data():
+    fashion_mnist = keras.datasets.fashion_mnist
+    (train_images, _), (_, _) = fashion_mnist.load_data()
+    train_images = train_images / 255
+    return train_images, train_images.shape[0]
+
+# leaky relu - rectified linear unit
+def lrelu(x, th=0.2):
+    return tf.maximum(th * x, x)
+
+# Generator G(x)
+def generator(x, isTrain=True, reuse=False):
+    with tf.variable_scope('generator', reuse=reuse):
+        # 1st hidden layer
+        conv1 = tf.layers.conv2d_transpose(x, 1024, [4, 4], strides=(1, 1), padding='valid')
+        lrelu1 = lrelu(tf.layers.batch_normalization(conv1, training=isTrain), 0.2)
+
+        # 2nd hidden layer
+        conv2 = tf.layers.conv2d_transpose(lrelu1, 512, [4, 4], strides=(2, 2), padding='same')
+        lrelu2 = lrelu(tf.layers.batch_normalization(conv2, training=isTrain), 0.2)
+
+        # 3rd hidden layer
+        conv3 = tf.layers.conv2d_transpose(lrelu2, 256, [4, 4], strides=(2, 2), padding='same')
+        lrelu3 = lrelu(tf.layers.batch_normalization(conv3, training=isTrain), 0.2)
+
+        # 4th hidden layer
+        conv4 = tf.layers.conv2d_transpose(lrelu3, 128, [4, 4], strides=(2, 2), padding='same')
+        lrelu4 = lrelu(tf.layers.batch_normalization(conv4, training=isTrain), 0.2)
+
+        # output layer
+        conv5 = tf.layers.conv2d_transpose(lrelu4, 1, [4, 4], strides=(2, 2), padding='same')
+        return tf.nn.tanh(conv5)
+
+# Discriminator D(x)
+def discriminator(x, isTrain=True, reuse=False):
+    with tf.variable_scope('discriminator', reuse=reuse):
+        # 1st hidden layer
+        conv1 = tf.layers.conv2d(x, 128, [4, 4], strides=(2, 2), padding='same')
+        lrelu1 = lrelu(conv1, 0.2)
+
+        # 2nd hidden layer
+        conv2 = tf.layers.conv2d(lrelu1, 256, [4, 4], strides=(2, 2), padding='same')
+        lrelu2 = lrelu(tf.layers.batch_normalization(conv2, training=isTrain), 0.2)
+
+        # 3rd hidden layer
+        conv3 = tf.layers.conv2d(lrelu2, 512, [4, 4], strides=(2, 2), padding='same')
+        lrelu3 = lrelu(tf.layers.batch_normalization(conv3, training=isTrain), 0.2)
+
+        # 4th hidden layer
+        conv4 = tf.layers.conv2d(lrelu3, 1024, [4, 4], strides=(2, 2), padding='same')
+        lrelu4 = lrelu(tf.layers.batch_normalization(conv4, training=isTrain), 0.2)
+
+        # output layer
+        conv5 = tf.layers.conv2d(lrelu4, 1, [4, 4], strides=(1, 1), padding='valid')
+        o = tf.nn.sigmoid(conv5)
+
+        return o, conv5
